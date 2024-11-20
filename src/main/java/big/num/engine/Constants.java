5d69add854401cd5b458bc2529eea59c2a5ead95
Brandon Fowler <brandon@fivefowlers.com>
2024-11-20 10:51:31 -0700

    Add more constants, remove target folders

--- src/main/java/big/num/engine/Constants.java
+++ src/main/java/big/num/engine/Constants.java
@@ -5,6 +5,17 @@ import java.math.BigDecimal;
 public class Constants {
     public static BigDecimal e = new BigDecimal(Math.E);
     public static BigDecimal pi = new BigDecimal(Math.PI);
+    public static BigDecimal G = new BigDecimal(6.67430E-11);
+    public static BigDecimal c = new BigDecimal(299792458);
+    public static BigDecimal h = new BigDecimal(6.62607015E-34);
+    public static BigDecimal hbar = new BigDecimal(1.054571817E-34);
+    public static BigDecimal q = new BigDecimal(1.602176634E-19);
+    public static BigDecimal k = new BigDecimal(1.380649E-23);
+    public static BigDecimal Na = new BigDecimal(6.02214076E23);
+    public static BigDecimal R = new BigDecimal(8.314462618);
+    public static BigDecimal sigma = new BigDecimal(5.670374419E-8);
+    public static BigDecimal mu0 = new BigDecimal(4 * Math.PI * 1E-7);
+    public static BigDecimal epsilon0 = new BigDecimal(8.8541878128E-12);
 
     
 }
